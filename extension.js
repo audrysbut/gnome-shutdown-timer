970eda7900fc6b25b921c6626c9e65177ace01be
Audrius Butkus <audrysbut@gmail.com>
2026-04-11 09:51:01 +0300

    feat: add functionality to cancel scheduled shutdown and enhance button interaction

--- extension.js
+++ extension.js
@@ -12,7 +12,7 @@ ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
 const Gettext = imports.gettext;
 const _ = Gettext.domain(GETTEXT_DOMAIN).gettext;
 
-const { GObject, St } = imports.gi;
+const { GObject, Gio, St } = imports.gi;
 
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
@@ -20,13 +20,57 @@ const PanelMenu = imports.ui.panelMenu;
 const Indicator = GObject.registerClass(
   class Indicator extends PanelMenu.Button {
     _init() {
-      super._init(0.0, _("ShutdownTimer"));
+      super._init(0.0, _("ShutdownTimer"), true);
+
+      this._timer = null;
 
       this.button = new St.Button({
         style_class: "shutdownTimerPanel",
         label: "",
+        x_expand: true,
+        y_expand: true,
+      });
+      // Clicks hit this child St.Button; the parent PanelMenu.Button's
+      // vfunc_event is not invoked for events delivered to the child.
+      this.button.connect("clicked", () => {
+        if (!this._timer || this._timer.timerValue == null) return;
+        this._onTimerClicked();
       });
       this.add_child(this.button);
+    }
+
+    setShutdownTimer(timer) {
+      this._timer = timer;
+    }
+
+    _cancelSystemShutdown() {
+      try {
+        // Shell 42 / older GJS: makeForBusSync is unavailable; use new_for_bus_sync.
+        const proxy = Gio.DBusProxy.new_for_bus_sync(
+          Gio.BusType.SYSTEM,
+          Gio.DBusProxyFlags.NONE,
+          null,
+          "org.freedesktop.login1",
+          "/org/freedesktop/login1",
+          "org.freedesktop.login1.Manager",
+          null
+        );
+        proxy.call_sync(
+          "CancelScheduledShutdown",
+          null,
+          Gio.DBusCallFlags.NONE,
+          -1,
+          null
+        );
+        return true;
+      } catch (e) {
+        logError(e, "shutdown-timer: CancelScheduledShutdown failed");
+        return false;
+      }
+    }
+
+    _onTimerClicked() {
+      if (this._cancelSystemShutdown()) this._timer.setTimerValue(undefined);
     }
 
     setTimerValue(time) {
@@ -50,6 +94,7 @@ class Extension {
 
     const extractor = new ShutdownTimeExtractor();
     const timer = new Timer(this._indicator);
+    this._indicator.setShutdownTimer(timer);
     this.monitor = new ShutdownFileMonitor(extractor, timer);
     Main.panel.addToStatusArea(this._uuid, this._indicator);
     // Main.panel.addToStatusArea(this._uuid, this._indicator, 0, 'left');
